aa4af0707d2979a53d44ba7e7d08614e1e2fd276
Tim <chent1101@gmail.com>
2015-10-25 23:21:25 -0600

    dropping the jquery on this dizzo like it's hot butter on toast

--- js/js.js
+++ js/js.js
@@ -1,3 +1,49 @@
-/**
- * Created by MagicTime on 10/21/15.
- */
+$(document).ready(function() {
+    $('.clr').click(function() {
+        $('input').val('');
+    });
+
+    $('.add-button').on('click', function() {
+        if ($('input').val()) {
+            $('.list-items:first').before('<div class="list-items">' +
+                '<div class="ind-items"> <div class="check-box"></div>' +
+                '<h3 class="item-name"></h3> <div class="del"><h1>X</h1></div></div></div>');
+            $('.item-name:first').prepend($('input').val());
+            $('input').val('');
+        };
+    });
+
+    $('.left').hover(function() {
+        $(this).css({border: '2px solid lightgrey', cursor: 'pointer'});
+        $(this).siblings().css({border: '2px solid lightgrey', cursor: 'pointer'});
+    }, function() {
+        $(this).css({border: '1px solid lightgrey'});
+        $(this).siblings().css({border: '1px solid lightgrey'});
+    });
+
+    $('.right').hover(function() {
+        $(this).css({border: '2px solid lightgrey', cursor: 'pointer'});
+        $(this).siblings().css({border: '2px solid lightgrey', cursor: 'pointer'});
+    }, function() {
+        $(this).css({border: '1px solid lightgrey'});
+        $(this).siblings().css({border: '1px solid lightgrey'});
+    });
+
+    //check items
+    $('.list-container .list-phone .display .container .list-items').on('click', '.check-box', function() {
+        $(this).replaceWith('<div class="check">' +
+            '<div class="left"></div>' +
+            '<div class="right"></div>' +
+            '</div>');
+        $(this).child().after('<div class="line"></div>');
+        $(this).siblings('h3').css({color: 'lightgrey'});
+    });
+
+    //uncheck items
+    //$('div.container').on('click', '.check', function() {
+    //    $(this).replaceWith('<div class="check-box"></div>')
+    //    $(this).eq(1).remove();
+    //    $(this).siblings('h3').css({color: 'black'});
+    //});
+});
+
